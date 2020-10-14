68aa7cd04a0dea6fab18c3c6413389cda73a94da
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-10-15 02:16:45 +0530

    Writing Basic JSX

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 console.log(`Running`);
 
-const template = <h1>Indecision App</h1>;
+const template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is a Paragraph</p>
+        <ol>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+        </ol>
+    </div>
+);
+
 const appRoot = document.querySelector('#app');
 
 ReactDOM.render(template, appRoot);
