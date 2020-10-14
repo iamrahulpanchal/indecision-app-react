a455a27abd12c5ef7881b8206f786d2be9b9d8d8
iamrahulpanchal <rahulnpanchal50@gmail.com>
2020-10-15 02:20:33 +0530

    Setting Up Build Script

---
+++ public/sycripts/app.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log("Running");
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "Indecision App"), /*#__PURE__*/React.createElement("p", null, "This is a Paragraph"), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item 1"), /*#__PURE__*/React.createElement("li", null, "Item 2"), /*#__PURE__*/React.createElement("li", null, "Item 3")));
+var appRoot = document.querySelector('#app');
+ReactDOM.render(template, appRoot);
